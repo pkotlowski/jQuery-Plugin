1c404df9be769228987916f50cf8bd031082c45a
Piotr Kotłowski <piotr.kotlowski@gmail.com>
2014-05-08 02:48:42 +0200

    Postal code validation

--- js/newjavascript.js
+++ js/newjavascript.js
@@ -2,9 +2,9 @@
     "use strict";
 
     var validateText = function(value, minLength, maxLength, regExpression) {
-        var re= new RegExp(regExpression)
-        console.log(re)
-        console.log(re.test(value))
+        var re = new RegExp(regExpression);
+
+        console.log(value.length >= minLength && value.length <= maxLength && re.test(value));
         if (value.length >= minLength && value.length <= maxLength && re.test(value)) {
             return true;
         }
@@ -21,11 +21,104 @@
         }, options);
         //console.log(validateText(this.val(), settings.minimumLength, settings.maximumLength));
         if (validateText(this.val(), settings.minimumLength, settings.maximumLength, settings.regularExpression)) {
-            return this.parent().removeClass("has-error");
+            return true;
         }
         else {
-            return this.parent().addClass("has-error");
+            return false;
         }
+    };
+    var validateMail = function(value, minLength, maxLength, regExpression) {
+        var re = new RegExp(regExpression);
+        if (value.length >= minLength && value.length <= maxLength && re.test(value)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    $.fn.validateEmail = function(options) {
+        var settings = $.extend({
+            minimumLength: 0,
+            maximumLength: 99,
+            regularExpression: "^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]{1,})*\.([a-zA-Z]{2,}){1}$"
+        }, options);
+
+        if (validateMail(this.val(), settings.minimumLength, settings.maximumLength, settings.regularExpression)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    var getCityFromPostalCode = function(code) {
+        var city;
+        $.ajax({
+            type: "GET",
+            url: "/jQuery/Public/kody.csv",
+            dataType: "text",
+            async:false,
+            success: function(data) {
+                var dataa = $.csv.toObjects(data, {separator: ';'});
+                for (var i = 0; i < dataa.length; i++) {
+                    
+                    if(dataa[i].KOD_POCZTOWY===code){
+                        //console.log(dataa[i].MIEJSCOWOŚĆ)
+                        city=dataa[i].MIEJSCOWOŚĆ;
+                        //callback();
+                        
+                        //break;
+                    }
+                }
+            }          
+        
+        });
+        return city;
+    };
+    var validatePostalCode = function(value, regExpression) {
+        var re = new RegExp(regExpression);
+        if (re.test(value)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    $.fn.validatePostalCode = function(options) {
+        var settings = $.extend({
+            regularExpression: "[0-9][0-9]-[0-9][0-9][0-9]"
+        }, options);
+        if (validatePostalCode(this.val(), settings.regularExpression)) {
+            var cityName = getCityFromPostalCode(this.val());
+                return ({valid:true, cityName:cityName});
+                            
+            
+        }
+        else {
+            return ({valid:false, cityName:cityName});
+        }
+    };
+    
+    var validatePasswordStrength=function(password, softPassword, mediumPassword, hardPassword){
+        var soft = new RegExp(softPassword);
+        var medium = new RegExp(mediumPassword);
+        var hard = new RegExp(hardPassword);
+    };
+    
+    $.fn.validatePassword = function(options) {
+        var settings = $.extend({
+            softPasswordRegularExpression: "[0-9]*",
+            mediumPasswordRegularExpression: "[0-9a-zA-Z]*",
+            hardPasswordRegularExpression: "[0-9a-zA-Z]*",
+        }, options);
+//        if () {
+//            var cityName = getCityFromPostalCode(this.val());
+//                return ({valid:true, cityName:cityName});
+//                            
+//            
+//        }
+//        else {
+//            return ({valid:false, cityName:cityName});
+//        }
     };
 
 })(jQuery);
--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 $(function() {
 
     $("#nameTextBox").keyup(function() {
-
-        $(this).validateTextBox({minimumLength:5, maximumLength:10});
+        var a = $(this).validateTextBox({minimumLength: 5, maximumLength: 10});
+        if (a) {
+            $("#nameTextBox").parent().removeClass("has-error");
+            $("#sbmBtn").removeAttr("disabled");
+        }
+        else {
+            $("#nameTextBox").parent().addClass("has-error");
+            $("#sbmBtn").attr("disabled", "disabled");
+        }
     });
+    $("#emailTextBox").keyup(function() {
+        var a = $(this).validateEmail({minimumLength: 10});
+        if (a) {
+            $("#emailTextBox").parent().removeClass("has-error");
+            $("#sbmBtn").removeAttr("disabled");
+        }
+        else {
+            $("#emailTextBox").parent().addClass("has-error");
+            $("#sbmBtn").attr("disabled", "disabled");
+        }
+    });
+
+    $("#postalCodeTextBox").keyup(function() {
+        var a = $(this).validatePostalCode({});
+        //console.log(a)
+        if (a.valid && a.cityName !== 'undefined') {
+            $("#postalCodeTextBox").parent().removeClass("has-error");
+            $("#sbmBtn").removeAttr("disabled");
+            console.log(a.cityName)
+            $("#cityTextBox").val(a.cityName);
+
+        }
+        else {
+            $("#postalCodeTextBox").parent().addClass("has-error");
+            $("#sbmBtn").attr("disabled", "disabled");
+        }
+    });
+
 });
